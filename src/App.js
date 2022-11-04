d74f81a8a5b006c870784d1d22dc662c6046ac35
pavelkpetkov <pavelpetkov@mail.bg>
2022-11-04 18:16:15 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+
+
+function App() {
+
+  return (
+    <div className="container">
+      <Header title='Hello from props'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import Button from "./Button"
+
+const Header = ({title}) => {
+
+    const onClick = () => {
+        console.log('click');
+    }
+    
+  return (
+    <header className='header'>
+        <h1>{title}</h1>
+        <Button color='green' text='Add' onClick={onClick}/>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+    title: 'Default Title'
+}
+
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black'
+// }
+
+export default Header
