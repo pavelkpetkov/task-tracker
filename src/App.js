a40647cd4fccf02d3885adc58bf2444dd02bbe4b
pavelkpetkov <pavelpetkov@mail.bg>
2022-11-06 16:04:05 +0200

    added routing and styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
-
+import Footer from './components/Footer';
+import About from './components/About';
 
 function App() {
 
@@ -20,6 +22,13 @@ function App() {
 
   const fetchTasks = async () => {
     const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data;
+  }
+
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
     const data = await res.json()
 
     return data;
@@ -49,16 +58,37 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id));
   }
 
-  const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task));
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updatedTask)
+    })
+    const data = res.json();
+
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !data.reminder } : task));
   }
 
   return (
-    <div className="container">
-      <Header title='Task tracker' onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? < Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No tasks to show'}
-    </div>
+    <Router>
+      <div className="container">
+        <Header title='Task tracker' onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+        <Routes>
+          <Route path='/' exact element={
+            <>
+              {showAddTask && <AddTask onAdd={addTask} />}
+              {tasks.length > 0 ? (< Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No tasks to show')}
+            </>}
+          />
+          <Route path='/about' element={<About />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+
+    return (
+        <footer>
+            <p>Copyright &copy; 2022</p>
+            <Link to='/about'>about</Link>
+        </footer>
+    )
+
+}
+
+export default Footer;
